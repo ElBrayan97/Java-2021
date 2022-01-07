349097d3d336c482e0866d699f30fc3b927dd052
ElBrayan97 <49415822+ElBrayan97@users.noreply.github.com>
2022-01-07 02:04:12 -0300

    prácticas
    JAVA TE ODIO :D

--- Cap3 Libro/src/Clases/pruebaLibroCalificaciones.java
+++ Cap3 Libro/src/Clases/pruebaLibroCalificaciones.java
@@ -10,14 +10,23 @@ public class pruebaLibroCalificaciones {
 
 	public static void main(String[] args) {
 		
-		Scanner escanner = new Scanner(System.in);
+		Scanner object = new Scanner(System.in);
 		LibroCalificaciones miLibroCalificaciones = new LibroCalificaciones();
+		LibroCalificaciones miLibroCalificaciones1 = new LibroCalificaciones();
 		
-		System.out.print("Type the name of the course: ");
-		String nombre = escanner.nextLine();
+		System.out.print("Type the name of the course 0: ");
+		String nombre = object.nextLine(); // Creo y uso la variable
+		miLibroCalificaciones.setNombreDelCurso(nombre);
+		
+		System.out.print("Type the name of the course 1: ");
+		nombre = object.nextLine(); // la variable anteriormente creada la reutilizo
+		miLibroCalificaciones1.setNombreDelCurso(nombre);
 		System.out.println();
-		miLibroCalificaciones.establecerNombreDelCurso(nombre);
+		System.out.println("Libro 1:");
 		miLibroCalificaciones.mostrarMensaje();
+		System.out.println("Libro 2:");
+		miLibroCalificaciones1.mostrarMensaje();
+		object.close();
 		
 	}
 
