fa92bb7dfaf84ad78c9efefc52692a8f5debb624
ElBrayan97 <49415822+ElBrayan97@users.noreply.github.com>
2023-02-17 05:45:01 -0300

    update
    ahora vamo a empeza a jogar :P deah xD

--- Java-2022/src/tp5b/Tablero.java
+++ Java-2022/src/tp5b/Tablero.java
@@ -1,21 +1,33 @@
 package tp5b;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Collections;
 import java.util.Random;
+import java.util.HashMap;
 
 public class Tablero
 {
 	private ArrayList<Bote> botes;
+
+	private HashMap<Integer,String> equivalencia = new HashMap<Integer,String>(); 
 	
-	private void Inicializar() {
+	private void startEquivalencia() {
+		equivalencia.put(0, "A");
+		equivalencia.put(1, "B");
+		equivalencia.put(2, "C");
+		equivalencia.put(3, "D");
+		equivalencia.put(4, "E");
+		equivalencia.put(5, "F");
+		equivalencia.put(6, "G");
+	}
+	
+	public void Inicializar() {
 		Random rand = new Random();
 		for (int i=0;i==5;i++) {
 			int x = rand.nextInt(8);
 			int y = rand.nextInt(8);
 			botes.add(new Bote(x,y));
 			}
+		this.startEquivalencia();
 		}
 	
 	public Tablero() {
@@ -23,12 +35,20 @@ public class Tablero
 		Inicializar();
 		}
 	
-	public boolean searchBoat(int X ,int Y){
-		for(int i=0;i==botes.size();i++) {
-			
-			} 
+	public boolean searchBoat(int x, int y) // Verifica que las posiciones que se ingresaron, corresponden a un bote
+		{
+		for(int i=0; i==botes.size(); i++)
+			{
+			Bote b = new Bote(-1,-1);
+			b = botes.get(i);
+			if ((b.getColumna()==y) && (b.getFila()==x))
+				{
+				botes.remove(i);
+				return true;
+				}
+			}
+		return false;
 		}
-	
 	
 	public ArrayList<Bote> getBote(){
 		return botes;
--- Java-2022/src/tp5b/Test.java
+++ Java-2022/src/tp5b/Test.java
@@ -1,7 +1,24 @@
 package tp5b;
 
+import java.util.Scanner;
+
 public class Test {	
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) 
+	{
+		Tablero mapa = new Tablero(); 		// creacion d ela matriz/mapa
+		mapa.Inicializar();					//carga de laas posiciones que tienen barcos
+		for (int i=0;i>=24;i++)
+		{
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Ingrese una posicion 0-7");
+			int x = sc.nextInt();
+			System.out.println("Ingrese una posicion A-H");
+			String y = sc.next();
+			sc.close();
+			
+			mapa.searchBoat(aux, y)
+			
+			
+		}
 	}
 }
